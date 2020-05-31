bb225395888bc152896634dcf918d1670500b315
Raragyay <let987let987@gmail.com>
2020-05-30 21:32:54 -0400

    Restrucured for heroku

---
+++ api/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://raragkaizen:6H4J9FPtPJjRtQaP@cluster0-lyr9l.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: process.env.SECRET || "V@Zyn%nhjccZ^BG*1AFhXu5pcL!9MQ$x*UwILnNk2EuV4WN!H^"
+};
